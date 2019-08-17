142da5726e6e169cf2cb4bff8f2dffe5bf6a3dfd
VivekMajithiya <52253560+VivekMajithiya@users.noreply.github.com>
2019-08-18 01:40:28 +0530

    Update validate_selenium_result.sh

--- validate_selenium_result.sh
+++ validate_selenium_result.sh
@@ -3,17 +3,12 @@ cd /root/eduproject/projCert
 export result=`grep -i success output.txtbkp | wc -l`
 
 if [ $result -eq 1 ]
-  echo "all ok"
-
-fi
-
-if [ $result -eq 1 ]
 then
    echo "Selenium test successful"
 else
    echo "Selenium test failed stopping and deleting container"
- #  /usr/bin/docker stop $(docker ps -a -q)
- #  /usr/bin/docker rm $(docker ps -a -q) --force
+   /usr/bin/docker stop $(docker ps -a -q)
+   /usr/bin/docker rm $(docker ps -a -q) --force
 fi
 rm -f output.txtbkp
 mv output.txt output.txtbkp
